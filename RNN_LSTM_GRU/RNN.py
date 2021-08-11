e3d9e29c7c3157494dc453ab0d24010ea8e7f96b
Xie Zipeng <53039617+xiezipeng-ML@users.noreply.github.com>
2021-08-11 20:40:04 +0800

    Add files via upload

---
+++ RNN_LSTM_GRU/RNN.py
@@ -0,0 +1,156 @@
+import oneflow as flow
+from oneflow import nn
+from oneflow import Tensor
+from oneflow.nn import Parameter
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers=1, nonlinearity='tanh', bias=True, batch_first=False, dropout=0, bidirectional=False):
+        super().__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.nonlinearity = nonlinearity
+        self.bias = bias
+        self.batch_first = batch_first
+        self.dropout = dropout
+        self.bidirectional = bidirectional
+        num_directions = 2 if bidirectional else 1
+        gate_size = hidden_size
+        self.drop = nn.Dropout(self.dropout)
+
+        if self.nonlinearity == 'tanh':
+            self.act = nn.Tanh()
+        elif self.nonlinearity == 'relu':
+            self.act = nn.LeakyReLU()
+        else:
+            raise ValueError("Unknown nonlinearity '{}'".format(self.nonlinearity))
+
+
+        for layer in range(num_layers):
+            for direction in range(num_directions):
+                
+                real_hidden_size = hidden_size
+                layer_input_size = input_size if layer == 0 else real_hidden_size * num_directions 
+
+                w_ih = Parameter(Tensor(gate_size, layer_input_size))
+                w_hh = Parameter(Tensor(gate_size, real_hidden_size))
+                b_ih = Parameter(Tensor(gate_size))
+                b_hh = Parameter(Tensor(gate_size))
+
+                layer_params = ()
+                
+                if bias:
+                    layer_params = (w_ih, w_hh, b_ih, b_hh)
+                else:
+                    layer_params = (w_ih, w_hh)
+
+                suffix = '_reverse' if direction ==1 else ''
+                param_names = ['weight_ih_l{}{}', 'weight_hh_l{}{}']
+                if bias:
+                    param_names += ['bias_ih_l{}{}', 'bias_hh_l{}{}']
+                param_names = [x.format(layer, suffix) for x in param_names]
+
+                for name, param in zip(param_names, layer_params):
+                    setattr(self, name, param)
+        
+        self.reset_parameters()
+
+
+    def reset_parameters(self):
+        for param in self.parameters():
+            if param.dim() > 1:
+                nn.init.xavier_uniform_(param)
+    
+    def permute_tensor(self, input):
+        return input.permute(1,0,2)
+
+    def forward(self, x, hidden=None):
+        if self.batch_first == False:
+            x = self.permute_tensor(x)
+            
+        D = 2 if self.bidirectional else 1
+        num_layers = self.num_layers
+        batch_size, seq_len, _ = x.size()
+
+        if hidden is None:
+            h_t = flow.zeros((D * num_layers, batch_size, self.hidden_size), dtype=x.dtype, device=x.device)
+        else:
+            h_t = hidden
+
+        if self.bidirectional:
+            h_t_f = flow.cat([h_t[l, :, :].unsqueeze(0) for l in range(h_t.size(0)) if l%2==0],dim=0)
+            h_t_b = flow.cat([h_t[l, :, :].unsqueeze(0) for l in range(h_t.size(0)) if l%2!=0],dim=0)
+        else:
+            h_t_f = h_t
+        
+        layer_hidden = []       
+
+        for layer in range(self.num_layers):
+            hidden_seq_f = []      
+            if self.bidirectional:
+                hidden_seq_b = []  
+
+            hid_t_f = h_t_f[layer, :, :]            
+            if self.bidirectional:
+                hid_t_b = h_t_b[layer, :, :]
+            
+            for t in range(seq_len):
+                if layer == 0:      
+                    x_t_f = x[:, t, :]   
+                    if self.bidirectional:
+                        x_t_b = x[:, seq_len-1-t, :]  
+                else:
+                    x_t_f = hidden_seq[:, t, :] 
+                    if self.bidirectional:
+                        x_t_b = hidden_seq[:, seq_len-1-t, :]
+
+                hy1_f = flow.matmul(x_t_f, getattr(self,'weight_ih_l{}{}'.format(layer,'')).permute(1,0))
+                hy2_f = flow.matmul(hid_t_f, getattr(self, 'weight_hh_l{}{}'.format(layer,'')).permute(1,0))
+                if self.bias:
+                    hy1_f += getattr(self, 'bias_ih_l{}{}'.format(layer,''))
+                    hy2_f += getattr(self, 'bias_hh_l{}{}'.format(layer,''))
+                hid_t_f = self.act(hy1_f + hy2_f)
+
+                hidden_seq_f.append(hid_t_f.unsqueeze(1))
+
+                if self.bidirectional:
+                    hy1_b = flow.matmul(x_t_b, getattr(self,'weight_ih_l{}{}'.format(layer,'_reverse')).permute(1,0))
+                    hy2_b = flow.matmul(hid_t_b, getattr(self, 'weight_hh_l{}{}'.format(layer,'_reverse')).permute(1,0))
+                    if self.bias:
+                        hy1_b += getattr(self, 'bias_ih_l{}{}'.format(layer,'_reverse'))
+                        hy2_b += getattr(self, 'bias_hh_l{}{}'.format(layer,'_reverse'))
+                    hid_t_b = self.act(hy1_b + hy2_b)
+
+                    hidden_seq_b.insert(0, hid_t_b.unsqueeze(1))
+                
+
+            hidden_seq_f = flow.cat(hidden_seq_f, dim=1)    
+            if self.bidirectional:
+                hidden_seq_b = flow.cat(hidden_seq_b, dim=1)    
+            
+            if self.dropout != 0 and layer != self.num_layers-1:
+                hidden_seq_f = self.drop(hidden_seq_f)
+                if self.bidirectional:
+                    hidden_seq_b = self.drop(hidden_seq_b)
+
+            if self.bidirectional:
+                hidden_seq = flow.cat([hidden_seq_f,hidden_seq_b],dim=2)    
+            else:
+                hidden_seq = hidden_seq_f
+
+            if self.bidirectional:
+                h_t = flow.cat([hid_t_f.unsqueeze(0), hid_t_b.unsqueeze(0)], dim=0)  
+            else:
+                h_t = hid_t_f.unsqueeze(0)
+
+            layer_hidden.append(h_t)        
+            
+        h_t = flow.cat(layer_hidden, dim=0)    
+        
+        if self.batch_first == False:
+            hidden_seq = self.permute_tensor(hidden_seq)
+
+        return hidden_seq, h_t
+
